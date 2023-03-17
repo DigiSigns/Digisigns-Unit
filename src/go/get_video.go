b4858f46ec9e9c1a8325472156eceb8aff09f857
Cooper Vandiver <rvandiver@una.edu>
2023-03-17 18:42:09 -0500

    foo

--- src/go/get_video.go
+++ src/go/get_video.go
@@ -6,24 +6,21 @@ import (
 	"log"
 	"os"
 	"sync"
+	"time"
+	"strconv"
 
 	grab "github.com/cavaliergopher/grab/v3"
 	_ "github.com/lib/pq"
 )
 
-// Struct to hold query results
-type supabase_item struct {
-	bucket string
-	name   string
-}
-
 // Constants (no magic values!)
 const (
 	dumpDirectory             = "./data/video_input_dump"
-	supabaseBucketUrlTemplate = "https://udlsgnjteywvklskdwvx.supabase.co/storage/v1/object/public/%s/%s"
 )
 
 func main() {
+	UID := os.Getenv("uid")
+	SID := os.Getenv("sid")
 	// Getting database connection
 	connStr := "user=postgres password=DigiSigns1029 host=db.udlsgnjteywvklskdwvx.supabase.co port=5432 dbname=postgres"
 	db, err := sql.Open("postgres", connStr)
@@ -32,20 +29,20 @@ func main() {
 	}
 
 	// Querying for necessary videos.
-	//rows, err := db.Query("SELECT bucket_id, name FROM storage.objects WHERE name like 'Geico%' ORDER BY name;")
-	rows, err := db.Query("SELECT bucket_id, name FROM storage.objects WHERE name like 'Geico%' OR name LIKE 'Y2%' ORDER BY name;")
+	//rows, err := db.Query("SELECT bucket_id, name FROM storage.objects WHERE name like 'Geico%' OR name LIKE 'Y2%' ORDER BY name;")
+	rows, err := db.Query("SELECT video_url FROM orders WHERE start_date='"+getCurrTime()+"' AND device_id_pk=(SELECT id FROM devices WHERE devices.host_id="+SID+" AND devices.device_id="+UID+");")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
 	// setting up query result variables
-	var obj supabase_item
-	obj_list := make([]supabase_item, 0)
+	var obj string
+	obj_list := make([]string, 0)
 
 	// collecting query results
 	for rows.Next() {
-		if err := rows.Scan(&obj.bucket, &obj.name); err != nil {
+		if err := rows.Scan(&obj); err != nil {
 			log.Fatal(err)
 		}
 		obj_list = append(obj_list, obj)
@@ -57,20 +54,18 @@ func main() {
 	// Using "Grab" package to concurrently download videos (no time to waste!)
 	for _, currObj := range obj_list {
 		// Starting anonymous function on separate thread
-		go func(obj supabase_item) {
+		go func(obj string) {
 			// Decrementing wg upon exit
 			defer wg.Done()
 
-			// formatting url
-			url := fmt.Sprintf(supabaseBucketUrlTemplate, obj.bucket, obj.name)
-
 			// downloading video
-			_, err := grab.Get(dumpDirectory, url)
+			_, err := grab.Get(dumpDirectory, obj)
 
 			// handling errors
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "%s\n", err)
 			}
+
 			// indicating that subprocess exited normally
 			fmt.Fprint(os.Stdout, "Goroutine exited normally.\n")
 		}(currObj)
@@ -79,4 +74,14 @@ func main() {
 	// Waiting for completion of goroutines
 	wg.Wait()
 
+}
+
+func getCurrTime() string {
+	t := time.Now()
+	s := strconv.FormatInt(int64(t.Year()), 10)
+	s += "-"
+	s += strconv.FormatInt(int64(t.Month()), 10)
+	s += "-"
+	s += strconv.FormatInt(int64(t.Day()), 10)
+	return s
 }
