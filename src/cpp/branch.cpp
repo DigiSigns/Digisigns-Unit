b1ff8130497b8be27727f3f604f88f833164bbc8
Cooper Vandiver <rvandiver@una.edu>
2023-04-06 19:15:12 -0500

    Added comments to branch, redid some debugging changes

--- src/cpp/branch.cpp
+++ src/cpp/branch.cpp
@@ -11,20 +11,21 @@
 void
 backgroundProcessLoop()
 {
-    auto const state = state_singleton::get_instance();
-    
-
     std::this_thread::sleep_for(std::chrono::seconds(4));
     system("pacmd suspend 1");
     std::this_thread::sleep_for(std::chrono::seconds(1));
     system("pacmd suspend 0");
+	// Initial status update below
     while(true)
     {
 #ifndef DEBUG
 		system("sudo ip link set wlan0 down");
+#endif
         std::this_thread::sleep_for(std::chrono::hours(1));
+#ifndef DEBUG
 		system("sudo ip link set wlan0 up");
 #endif
+		// Put status update code below (above preprocessor condition)
     }
 }
 
